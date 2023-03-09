c9a1e2b3959c8a507eec71c15a303dc8af170ecb
beka shamugia <68068249+beqa1414@users.noreply.github.com>
2023-03-09 19:46:59 +0400

    add rating

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import { useState } from 'react';
-import './App.css';
-import Rating from './Components/Rating'
-import RatingSubmit from './Components/RatingSubmit';
-
+import { useState } from "react";
+import "./App.css";
+import Rating from "./Components/Rating";
+import RatingSubmit from "./Components/RatingSubmit";
 
 function App() {
-  const [selectedNum,setSelectNum] = useState(null)
-  const [selected,setIsSelected] = useState(false)
+  const [selectedNum, setSelectNum] = useState(null);
+  const [selected, setIsSelected] = useState(false);
+
+  const selectNum = (e) => {
+    setSelectNum(e.target.innerText);
+  };
   return (
-   
     <div className="App">
-       {!selected && (
-      <Rating
-      selectedNum={selectedNum}
-      setSelectedNum={setSelectNum}
-      setIsSelected={setIsSelected}
-      
-    /> )}
+      {!selected && (
+        <Rating
+          selectedNum={selectedNum}
+          selectNum={selectNum}
+          setIsSelected={setIsSelected}
+        />
+      )}
 
-{selected && <RatingSubmit selectedNum={selectedNum}/>}
-     
-
+      {selected && <RatingSubmit selectedNum={selectedNum} />}
     </div>
-    
-  
   );
 }
 
